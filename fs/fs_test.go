a1157f91a4fa0d3afc15d65c4cf0bff44d70ae3c
Nicolas A Perez <anicolaspp@gmail.com>
2021-10-21 22:57:54 -0700

    some test examples for guidance

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -1,1 +1,33 @@
 package fs
+
+import "testing"
+
+// This is how Go tests are structured
+func TestSomethingMeaningless(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		a        int
+		b        int
+		expected int
+	}{
+		{
+			name:     "simple sum",
+			a:        5,
+			b:        3,
+			expected: 8,
+		},
+		{
+			name:     "sum with 0",
+			a:        5,
+			b:        0,
+			expected: 5,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			got := tc.a + tc.b
+			if got != tc.expected {
+				t.Errorf("Unexpected value, Got = %v, Want = %v", got, tc.expected)
+			}
+		})
+	}
+}
