0b65fff5d226f06b641698c5a320bb8c0768e023
Nicolas A Perez <anicolaspp@gmail.com>
2021-10-24 16:52:47 -0700

    client to connect to gftp over grpc

--- cli/cli.go
+++ cli/cli.go
@@ -1,2 +1,39 @@
 // Package cli contains the cli implementation to connect to our server.
-package cli
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "com.github.anicolaspp/gftp/protos"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "gftp-329818.uc.r.appspot.com:50051"
+	defaultName = "world"
+)
+
+func main() {
+	log.Println("Running...")
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	log.Println("Connection open...")
+	c := pb.NewGftpClient(conn)
+
+	req := &pb.StatusRequest{
+		Message: "nick",
+	}
+
+	log.Println("Making request...")
+	res, err := c.Status(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error communicating with %s, %v", address, err)
+	}
+
+	log.Printf("Response from server: %s", res)
+}
