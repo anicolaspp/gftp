8e95c284f964be9b58b451991408a7ae11679009
Nicolas A Perez <anicolaspp@gmail.com>
2021-10-23 22:58:13 -0700

    A simple webserver to make sure that it runs in AppEngine

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("I am alive!")
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	t := time.Now()
+
+	fmt.Fprint(w, t)
 }
